923b1b9129b4475c93db9dfb9ecbdfdef7be5fe1
pagecloudacedamy <paged.cloud@gmail.com>
2022-05-02 01:33:29 +0600

    initial commit

---
+++ php/index.php
@@ -0,0 +1,12 @@
+<?php
+$host = 'db';
+$user = 'page';
+$password = 'page';
+$db = 'page';
+$conn = new mysqli($host,$user,$password,$db);
+if($con->connect_error){
+    echo '!!!! faild !!!!!' . $conn->connect_error;
+
+}
+echo ' Welcome to Page Cloud Acedamy  ';
+?>
